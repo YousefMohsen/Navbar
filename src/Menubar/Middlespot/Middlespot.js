b59a273e536269298c9a14b2d9f06de93a697b3a
Yousef Mohsen <YM@MacBook-Pro-113.local>
2018-12-29 00:04:27 +0100

    first commit

---
+++ src/Menubar/Middlespot/Middlespot.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class Middlespot extends Component {
+  render() {
+    return (
+      <div className="">
+    Middlespot goes her
+      </div>
+    );
+  }
+}
+
+export default Middlespot;
---
+++ src/Menubar/Topbar/Topbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Middlespot from '../Middlespot/Middlespot'
+class Topbar extends Component {
+  render() {
+    return (
+      <div className="">
+   Topbar goes here
+    <Middlespot/>
+    Subbar goes here
+      </div>
+    );
+  }
+}
+
+export default Topbar;
